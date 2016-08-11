099f5192bd4184f9ae69819d66272b328572cfd7
JuanMiguel <juancabrera.dev@gmail.com>
2016-08-11 17:44:55 +0200

    Hero

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent }  from './app.component';
+import { DummyComponent }  from './dummy/dummy.component';
+
+@NgModule({
+  imports: [ 
+  	BrowserModule, 
+		FormsModule  	
+  ],
+  declarations: [
+  	AppComponent,
+  	DummyComponent
+  ],
+  bootstrap: [ 
+  	AppComponent 
+	]
+})
+
+export class AppModule { }
---
+++ app/dummy/dummy.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-dummy',
+	template: '<h1>Dummy</h1>'
+})
+
+export class DummyComponent { }
