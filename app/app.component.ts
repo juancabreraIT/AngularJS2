d238ff6f7178b2cd6e8a937ccfa59e703c3ffb76
Juan Miguel Cabrera <juancabrera.dev@gmail.com>
2016-08-13 19:47:55 +0200

    First component example

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: '/app/app.html',
+  styleUrls: ['/app/app.css']
+})
+
+export class AppComponent { }
