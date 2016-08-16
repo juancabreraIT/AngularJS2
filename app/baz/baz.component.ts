d959afa7674fbf3ee2c62b863f7f5d9a3d2140d2
JuanMiguel <juancabrera.dev@gmail.com>
2016-08-16 14:27:46 +0200

    Move 'SampleService' to baz folder

--- app/baz/baz.component.ts
+++ app/baz/baz.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { SampleService } from '../services/sample.service';
+import { SampleService } from './sample.service';
 
 @Component({
   selector: 'baz',
