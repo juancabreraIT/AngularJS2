ec09309ac5692d1b68f557ab652981d868353dca
JuanMiguel <juancabrera.dev@gmail.com>
2016-08-16 14:15:39 +0200

    Add a 'SampleService' to 'Baz' component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { DummyComponent } from './dummy/dummy.component';
 import { FooComponent } from './foo/foo.component';
+import { BazComponent } from './baz/baz.component';
 
 @Component({
   selector: 'app',
   templateUrl: '/app/app.html',
   directives: [
                 DummyComponent,
-                FooComponent                 
+                FooComponent,
+                BazComponent
               ],
   styleUrls: ['/app/app.css']
 })
