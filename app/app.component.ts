2d63b72434b00a791fe8201c2f1ff32ab0e9708e
JuanMiguel <juancabrera.dev@gmail.com>
2016-08-16 13:49:24 +0200

    Include 'Dummy' & 'Foo' components into 'app'

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { DummyComponent } from './dummy/dummy.component';
+import { FooComponent } from './foo/foo.component';
 
 @Component({
   selector: 'app',
   templateUrl: '/app/app.html',
+  directives: [
+                DummyComponent,
+                FooComponent                 
+              ],
   styleUrls: ['/app/app.css']
 })
 
